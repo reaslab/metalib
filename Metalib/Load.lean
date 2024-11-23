912a94d40c5c5ccdf6b8c6af3e36023100f15d81
Tony Beta Lambda <tonybetalambda@gmail.com>
2024-11-23 19:12:36 +0800

    refactor: minor modification to loader API

--- Metalib/Load.lean
+++ Metalib/Load.lean
@@ -7,21 +7,17 @@ import Lean
 
 open Lean Elab Parser System Frontend
 
-def handleHeader (header : Syntax) (messages : MessageLog) (inputCtx : InputContext) : IO Command.State := do
+def handleHeader (header : Syntax) (messages : MessageLog) (inputCtx : InputContext) (options : Options := {}) : IO Command.State := do
   initSearchPath (← findSysroot)
-  let (env, messages) ← processHeader (leakEnv := true) header .empty messages inputCtx
-  return Command.mkState env messages
+  let (env, messages) ← processHeader (leakEnv := true) header options messages inputCtx
+  return Command.mkState env messages options
 
 def loadFrontend (inputCtx : InputContext) (initState : Syntax → MessageLog → InputContext → IO Command.State := handleHeader) : IO (Context × State) := do
   let (header, parserState, messages) ← parseHeader inputCtx
   let commandState ← initState header messages inputCtx
   return ({ inputCtx }, { commandState, parserState, cmdPos := parserState.pos, commands := #[] })
 
-def loadFile (path : FilePath) : IO (Context × State) := do
+def loadFile (path : FilePath) (initState : Syntax → MessageLog → InputContext → IO Command.State := handleHeader) : IO (Context × State) := do
   let input ← IO.FS.readFile path
   let inputCtx := mkInputContext input path.toString
-  loadFrontend inputCtx
-
-def withFile {α : Type _} (path : FilePath) (m : FrontendM α) : IO (α × State) := do
-  let (context, state) ← loadFile path
-  m context |>.run state
+  loadFrontend inputCtx initState
