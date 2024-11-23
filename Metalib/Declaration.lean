f16171295454a1367beb32d8113a3b637fb21f61
Tony Beta Lambda <tonybetalambda@gmail.com>
2024-11-23 19:06:31 +0800

    doc: add copyright notice

--- Metalib/Declaration.lean
+++ Metalib/Declaration.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 BICMR@PKU. All rights reserved.
+Released under the Apache 2.0 license as described in the file LICENSE.
+Authors: Tony Beta Lambda
+-/
 import Lean
 
 open Lean Elab Meta Parser PrettyPrinter Tactic
